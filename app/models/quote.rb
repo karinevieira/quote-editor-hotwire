d7798dec075fa8b87a9ea0e7f33af5698d2d67f5
Karine Vieira <kv.vieira1@gmail.com>
2022-04-23 10:10:34 -0300

    ✨add a quote total

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,17 +2,16 @@ class Quote < ApplicationRecord
   belongs_to :company
 
   has_many :line_item_dates, dependent: :destroy
+
+  has_many :line_items, through: :line_item_dates
   
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
-
-  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
-  # after_update_commit -> { broadcast_replace_later_to "quotes" }
-  # after_destroy_commit -> { broadcast_remove_to "quotes" }
-  
-  # Those three callbacks are equivalent to the following single line
-  #broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
   
   broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
 end
